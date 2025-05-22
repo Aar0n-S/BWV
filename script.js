d2610088af7bceb504b5727203e2c45a577ed333
Aaron <Aaron2.Shanks@live.uwe.ac.uk>
2025-05-22 11:25:22 +0100

    split into separate files

---
+++ script.js
@@ -0,0 +1,25 @@
+//GSAP animation with staggered effect
+gsap.from(".leaf", {
+  rotationY: 180,
+  //scale:0,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.5,
+  ease: "power3.out"
+});
+
+//Add click event to trigger animation on individual leaves
+const leaves = document.querySelectorAll('.leaf');
+
+leaves.forEach(leaf => {
+  leaf.addEventListener('click', () => {
+    gsap.to(leaf, {
+      transformOrigin: "25% 25%",
+      rotationY: 180, // Rotate the leaf on click
+      y: -50,//move upwards
+      //scale:1.2, //slightly enlarge the leaf 
+      duration: 1,//duration of the animation 
+      ease: 'power1.inOut'//smoothing easing for rotation and movement
+    });
+  });
+});
