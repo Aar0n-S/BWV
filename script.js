308d66a90799ea83db32ead202deae767f758ad8
Aaron <Aaron2.Shanks@live.uwe.ac.uk>
2025-05-22 12:49:25 +0100

    tree added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let flipped = false;
+
 //GSAP animation with staggered effect
 gsap.from(".leaf", {
   rotationY: 180,
-  //scale:0,
+  scale:0,
   opacity: 0,
   duration: 1,
   stagger: 0.5,
@@ -13,12 +15,16 @@ const leaves = document.querySelectorAll('.leaf');
 
 leaves.forEach(leaf => {
   leaf.addEventListener('click', () => {
+    flipped = true;
+    console.log(flipped);
+    
     gsap.to(leaf, {
       transformOrigin: "25% 25%",
       rotationY: 180, // Rotate the leaf on click
       y: -50,//move upwards
       //scale:1.2, //slightly enlarge the leaf 
       duration: 1,//duration of the animation 
+      repeat:2,
       ease: 'power1.inOut'//smoothing easing for rotation and movement
     });
   });
