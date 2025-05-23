13158a373bc725505c3996770197b6ccbdfb23ef
Aaron <Aaron2.Shanks@live.uwe.ac.uk>
2025-05-23 13:06:40 +0100

    leaf flips back when clicked

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let flipped = false;
 //GSAP animation with staggered effect
 gsap.from(".leaf", {
   rotationY: 180,
-  scale:0,
+  scale: 0,
   opacity: 0,
   duration: 1,
   stagger: 0.5,
@@ -15,17 +15,28 @@ const leaves = document.querySelectorAll('.leaf');
 
 leaves.forEach(leaf => {
   leaf.addEventListener('click', () => {
-    flipped = true;
-    console.log(flipped);
-    
-    gsap.to(leaf, {
-      transformOrigin: "25% 25%",
-      rotationY: 180, // Rotate the leaf on click
-      y: -50,//move upwards
-      //scale:1.2, //slightly enlarge the leaf 
-      duration: 1,//duration of the animation 
-      //repeat:2,
-      ease: 'power1.inOut'//smoothing easing for rotation and movement
-    });
+    if (flipped == false) {
+      gsap.to(leaf, {
+        transformOrigin: "25% 25%",
+        rotationY: 180, // Rotate the leaf on click
+        y: -50,//move upwards
+        //scale:1.2, //slightly enlarge the leaf 
+        duration: 1,//duration of the animation 
+        //repeat:2,
+        ease: 'power1.inOut'//smoothing easing for rotation and movement
+      });
+      flipped = true;
+      console.log(flipped);
+    } else {
+      gsap.to(leaf, {
+        transformOrigin: "25% 25%",
+        rotationY: 360,
+        y: 50,
+        duration: 1,
+        ease: 'power1.inOut'//smoothing easing for rotation and movement
+      });
+      flipped = false;
+      console.log(flipped);
+    }
   });
 });
