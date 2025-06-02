3915322563286fb826f7d3801d5f5a553a900c1b
Aaron <Aaron2.Shanks@live.uwe.ac.uk>
2025-06-02 11:56:35 +0100

    moved leaf position statement

--- script.js
+++ script.js
@@ -5,32 +5,35 @@ let currFlipped = null;
 
 const container = document.querySelector(".backgroundContainer");
 
-const leafPositions = [];
+let leafPositions;
+
 const msgBoxes = document.querySelectorAll(".bwv-display-message-box");
 
 leaves.forEach((leaf, idx) => {
-    leafPositions.push(
-        {
-            top: (gsap.getProperty(leaf, "top") / gsap.getProperty(container, "height")) * 100,
-            left: (gsap.getProperty(leaf, "left") / gsap.getProperty(container, "width")) * 100,
-            right: (gsap.getProperty(leaf, "right") / gsap.getProperty(container, "width")) * 100
-        }
-    );
     leaf.addEventListener('click', () => {
         const txt = msgBoxes[idx];
-        console.log(leafPositions[idx].top);
 
         if (currFlipped == null || currFlipped == idx) {
-            if (currFlipped == null) {currFlipped = idx}
-            else {currFlipped = null}
+            if (currFlipped == null) { currFlipped = idx }
+            else { currFlipped = null }
+
+            if (currFlipped == idx) {
+                leafPositions = {
+                    top: (gsap.getProperty(leaf, "top") / gsap.getProperty(container, "height")) * 100,
+                    left: (gsap.getProperty(leaf, "left") / gsap.getProperty(container, "width")) * 100,
+                    right: (gsap.getProperty(leaf, "right") / gsap.getProperty(container, "width")) * 100
+                };
+
+                console.log(leafPositions.top);
+            }
 
             gsap.to(leaf, {
                 scale: flipped ? 1 : 7.5,
                 rotationY: flipped ? 0 : 180, // Rotate the leaf on click
                 rotationZ: flipped ? 0 : -45,
-                top: flipped ? `${leafPositions[idx].top}%` : "50%",
-                left: flipped ? `${leafPositions[idx].left}%` : "50%",
-                right: flipped ? `${leafPositions[idx].right}%` : "50%",
+                top: flipped ? `${leafPositions.top}%` : "50%",
+                left: flipped ? `${leafPositions.left}%` : "50%",
+                right: flipped ? `${leafPositions.right}%` : "50%",
                 xPercent: flipped ? 0 : -50,
                 yPercent: flipped ? 0 : -50,
                 zIndex: flipped ? 0 : 999999,
