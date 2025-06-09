eb7e356d99295c1ba2d4012cf3931da48ec9085f
Robin <robin.code22@gmail.com>
2025-06-09 11:32:27 +0100

    refactor: refactor js

--- script.js
+++ script.js
@@ -1,53 +1,175 @@
-const leaves = document.querySelectorAll('.leaf');
-
-let flipped = false;
-let currFlipped = null;
-
-const container = document.querySelector(".backgroundContainer");
-
-let leafPositions;
-
-const msgBoxes = document.querySelectorAll(".bwv-display-message-box");
-
-leaves.forEach((leaf, idx) => {
-    leaf.addEventListener('click', () => {
-        const txt = msgBoxes[idx];
-
-        if (currFlipped == null || currFlipped == idx) {
-            if (currFlipped == null) { currFlipped = idx }
-            else { currFlipped = null }
-
-            if (currFlipped == idx) {
-                leafPositions = {
-                    top: (gsap.getProperty(leaf, "top") / gsap.getProperty(container, "height")) * 100,
-                    left: (gsap.getProperty(leaf, "left") / gsap.getProperty(container, "width")) * 100,
-                    right: (gsap.getProperty(leaf, "right") / gsap.getProperty(container, "width")) * 100
-                };
-
-                console.log(leafPositions.top);
+const ANIMATION_DURATION = 1.25;
+class Leaf extends HTMLElement {
+    static CONFIG = {
+        states: {
+            default: {
+                scale: 1,
+                rotation: {x: 0, y: 0, z: 0},
+                position: {top: 0, left: 0},
+                zIndex: 0
+            },
+            expanded: {
+                scale: 7.5,
+                rotation: {x: 180, y: 180, z: 45},
+                position: {top: '50%', left: '50%'},
+                zIndex: 2,
             }
+        },
+        animation: {
+            duration: ANIMATION_DURATION,
+            ease: 'sine.inOut'
+        },
+    };
 
-            gsap.to(leaf, {
-                scale: flipped ? 1 : 7.5,
-                rotationY: flipped ? 0 : 180, // Rotate the leaf on click
-                rotationZ: flipped ? 0 : -45,
-                top: flipped ? `${leafPositions.top}%` : "50%",
-                left: flipped ? `${leafPositions.left}%` : "50%",
-                right: flipped ? `${leafPositions.right}%` : "50%",
-                xPercent: flipped ? 0 : -50,
-                yPercent: flipped ? 0 : -50,
-                zIndex: flipped ? 0 : 999999,
-                duration: 1,//duration of the animation 
-                ease: 'power1.inOut',//smoothing easing for rotation and movement
-                onComplete: () => {
-                    if (flipped) txt.style.display = 'initial'
-                },
-                onStart: () => {
-                    if (!flipped) txt.style.display = "none"
-                },
-            });
+    static LEAVES = Array.from(document.querySelectorAll('button[is="leaf-element"]'));
 
-            flipped = !flipped;
+    constructor() {
+        super();
+        this.initialPosition = null;
+        this.initialRotation = null;
+    }
+
+    connectedCallback() {
+        this.initialiseEventListeners();
+        // Initialise with collapsed state
+        this.setAttribute('data-expanded', 'false');
+        gsap.set(this, {
+            xPercent: -50,
+            yPercent: -50
+        });
+
+    }
+
+    initialiseEventListeners() {
+        this.addEventListener('click', () => this.handleLeafClick());
+        window.addEventListener('resize', () => this.handleResize());
+    }
+
+    handleLeafClick() {
+        if (this.initialPosition === null || this.initialRotation === null) this.initPositioning();
+
+        // Don't allow expansion if another leaf is already expanded
+        if (!this.isExpanded && Leaf.LEAVES.some(leaf => leaf.isExpanded)) return;
+
+        const config = this.getConfigurationForState();
+        this.toggleExpansion();
+        this.animateLeaf(config);
+    }
+
+    handleResize() {
+        this.setAttribute('data-expanded', 'false');
+
+        this.removeAttribute('style');
+
+        // Kill any ongoing animations
+        gsap.killTweensOf(this)
+
+        // Clear GSAP inline styles
+        gsap.set(this, {
+            clearProps: "all",
+        });
+
+        gsap.set(this, {
+            xPercent: -50,
+            yPercent: -50
+        });
+
+
+        // Force a reflow
+        this.offsetHeight;
+
+        // Reset initial values
+        this.initialPosition = null;
+        this.initialRotation = null;
+
+        // Reinitialise positioning
+        this.initPositioning();
+    }
+
+
+    get isExpanded() {
+        return this.getAttribute('data-expanded') === 'true';
+    }
+
+    toggleExpansion() {
+        this.setAttribute('data-expanded', (!this.isExpanded).toString());
+    }
+
+    initPositioning() {
+        this.initialPosition = this.calculateInitialPosition(this);
+        this.initialRotation = this.calculateInitialRotation(this);
+    }
+
+    calculateInitialPosition(element) {
+        const tree = document.querySelector('.tree');
+        return {
+            top: `${(gsap.getProperty(element, 'top') / gsap.getProperty(tree, 'height')) * 100}%`,
+            left: `${(gsap.getProperty(element, 'left') / gsap.getProperty(tree, 'width')) * 100}%`
+        };
+    }
+
+    calculateInitialRotation(element) {
+        return {
+            x: gsap.getProperty(element, 'rotationX') ?? 0,
+            y: gsap.getProperty(element, 'rotationY') ?? 0,
+            z: gsap.getProperty(element, 'rotationZ') ?? 0
+        };
+    }
+
+    getConfigurationForState() {
+        const config = this.isExpanded ? Leaf.CONFIG.states.default : Leaf.CONFIG.states.expanded;
+        if (this.isExpanded) {
+            config.position = this.initialPosition;
+            config.rotation = this.initialRotation;
         }
-    })
+        return {...config, animation: Leaf.CONFIG.animation};
+    }
+
+    scrollToLeaf() {
+        const rect = this.getBoundingClientRect();
+        const elementCenter = rect.top + window.scrollY + (rect.height / 2);
+        const windowCenter = window.innerHeight / 2;
+        const scrollToPosition = elementCenter - windowCenter;
+
+        window.scrollTo({
+            top: scrollToPosition,
+            behavior: 'smooth'
+        });
+    }
+
+    animateLeaf(config) {
+        gsap.to(this, {
+            scale: config.scale,
+            rotationX: config.rotation.x,
+            rotationY: config.rotation.y,
+            rotationZ: config.rotation.z,
+            top: config.position.top,
+            left: config.position.left,
+            ...config.animation,
+            onStart: () => {
+                if (this.isExpanded) this.style.zIndex = config.zIndex;
+            },
+            onComplete: () => {
+                // if (this.isExpanded) this.scrollToLeaf();
+                if (!this.isExpanded) this.style.zIndex = config.zIndex;
+            }
+        });
+    }
+}
+
+customElements.define('leaf-element', Leaf);
+
+const messages = document.querySelectorAll('.bwv-display-message-box');
+
+Leaf.LEAVES.forEach((leaf, idx) => {
+    leaf.addEventListener('click', () => {
+        if (messages[idx].classList.contains('active')) {
+            messages[idx].classList.toggle('active');
+        } else {
+            setTimeout(() => {
+                messages[idx].classList.toggle('active');
+            }, ANIMATION_DURATION * 1000)
+        }
+
+    });
 });
